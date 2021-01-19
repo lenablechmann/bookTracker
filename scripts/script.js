6043c0b3b7b4638eee15a79735a32bf2772080bb
Lena Blechmann <lenablechmann@gmail.com>
2021-01-19 15:28:34 +0100

    plan out code structure with pseudocode

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+// Check if DOM is loaded
+
+// get local Library string
+// parse it into an array of objects
+// if it doesn't exist, create an empty one
+
+// Loop through the array and append cards to the html
+// make sure each card has a data attribute that corresponds to the array index
+
+// eventlistener to the add new book button: 
+// call addBookToLib() which in turn
+// makes use of the Book constructor
+// storeBookInLocal
+
+// eventlistener onclick reading status,
+// changes boolean status in array read:true to false and vice versa
+
+// onclick delete eventlistener
+// deletes the corresponding object from the array and
+// calls the displayBookCards function
+
